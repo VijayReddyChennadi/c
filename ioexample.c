b15d26b5e84cafbfc5cb955185fcf8ebbe75aa10
VjAy Reddy Chennadi <vijay.reddy.chennadi@gmail.com>
2022-08-25 09:00:48 +0530

    Helloworld program

---
+++ ioexample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);  
+    printf("Number = %d",testInteger);
+    printf("\n");
+    printf("This is just testing VIM editor power, lets see");
+    printf("\n");
+    return 0;
+}
